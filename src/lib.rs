2110203929d7af44761e0d1223057354f6ff35bf
Dawid Lacheta <caushered@gmail.com>
2024-02-15 15:36:03 +0100

    handle incorrect input, create tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,26 @@
 use std::io;
 
 pub fn run() {
-    println!("Enter FIRST number: ");
-    let a = read_number();
-    println!("Enter OPERAND (+, -, * or /)");
-    let operand = read_user_input();
-    println!("Enter SECOND number: ");
-    let b = read_number();
+    loop {
+        println!("Enter FIRST number: ");
+        let mut a = read_number();
+        while a.is_none() {
+            a = read_number();
+        }
+        println!("Enter OPERAND (+, -, * or /)");
+        let operand = read_user_input();
+        println!("Enter SECOND number: ");
+        let mut b = read_number();
+        while b.is_none() {
+            b = read_number();
+        }
     
-    if a.is_none() || b.is_none() {
-        panic!("Incorrect value");
+        let n1 = a.unwrap();
+        let n2 = b.unwrap();
+        let result = calculate(n1, n2, &operand);
+    
+        println!("\n\n{n1} {operand} {n2} = {result}\n\n");
     }
-
-    let n1 = a.unwrap();
-    let n2 = b.unwrap();
-    let result = calculate(n1, n2, &operand);
-
-    print!("\n\n{n1} {operand} {n2} = {result}");
 }
 
 fn calculate(n1: f64, n2: f64, operand: &String) -> f64 {
@@ -49,6 +53,37 @@ fn read_number() -> Option<f64> {
     let str = read_user_input();
     match str.parse::<f64>() {
         Ok(value) => Some(value),
-        Err(err) => None
+        Err(err) => {
+            println!("Given value is not a valid number. Try again");
+            None
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    #[should_panic]
+    fn test_division_by_zero() {
+        let n1 = 18.0;
+        let operand = String::from("/");
+        let n2 = 0.0;
+        calculate(n1, n2, &operand);
+    }
+
+    #[test]
+    fn test_division() {
+        let n1 = 18.0;
+        let operand = String::from("/");
+        let n2 = 3.0;
+        assert_eq!(6.0, calculate(n1, n2, &operand));
+    }
+
+    #[test]
+    #[should_panic(expected = "Invalid operand!")]
+    fn not_valid_operand() {
+        calculate(5.0, 9.0, &String::from("?"));
     }
 }
